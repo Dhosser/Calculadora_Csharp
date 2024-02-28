391390d085a6569880e7363b8ba4810ce48b061b
Mr.Sas <80982769+Dhosser@users.noreply.github.com>
2024-02-28 01:50:22 -0300

    Add files via upload

---
+++ Calculadora/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        double first;
+        double second;
+        
+
+        Console.WriteLine("Primer numero:");
+        first = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo numero:");
+        second = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Elige la operacion (+, -, *, /):");
+        string operation = (Console.ReadLine());
+
+        double result;
+
+        switch (operation)
+        {
+            case "+":
+                result = first + second;
+                Console.WriteLine("{0} + {1} = {2}", first, second, result);
+            
+                break;
+                case "-":
+                    result = first - second;
+                    Console.WriteLine("{0} - {1} = {2}", first, second, result);
+
+                    break;
+                    case "*":
+                        result = first * second;
+                        Console.WriteLine("{0} * {1} = {2}", first, second, result);
+
+                        break;
+                        case "/":
+                            result = first / second;
+                            Console.WriteLine("{0} / {1} = {2}", first, second, result);
+
+                            break;
+            default:
+                Console.WriteLine("operacion invalida.");
+                break;
+        }
+    }
+}
